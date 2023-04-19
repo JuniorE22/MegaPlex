dfbbfcbd0e63b92848374a6a825f4f9638ff77d9
Jhonny <juniol2229@gmail.com>
2023-04-19 18:15:13 -0500

    filter to seacrh for name

--- megaPlex.js
+++ megaPlex.js
@@ -3,16 +3,26 @@ let GET_URL = "https://api.jikan.moe/v4/anime";
 let animeContainer = document.querySelector(".animeContainer")
 let containerTextCenter = document.querySelector("#containerText")
 let animeRow = document.querySelector("#rowContainer")
+const inputSearch = document.querySelector("#inputSearch") 
+const btnSearch = document.querySelector("#btnSearch")
 
+getAllAnimeData = async (params = {}) => {
+    const url = new URL(GET_URL);
 
-getAllAnimeData = async () => {
-    const response = await fetch(GET_URL)
+    Object.entries(params).forEach(([key, value]) =>
+
+    url.searchParams.set(key, value)
+    );
+
+    const response = await fetch(url.href)
         const {data} = await response.json();
         console.log(data);
         allAnimeData = data;
         
     }
    const createElements = () =>{
+    debugger
+    animeRow.innerHTML = "";
       allAnimeData.forEach(animeData => {
         let colDiv = document.createElement("div");
         let columnDiv = document.createElement("div");
@@ -25,7 +35,6 @@ getAllAnimeData = async () => {
         colDiv.className = "col"
         columnDiv.className = "card";
         columnDiv.style = "width: 13rem";
-        
         imgAnime.className = "card-img-top";
         imgAnime.src = animeData.images.webp.image_url;
         divDescription.className = "card-body";
@@ -35,12 +44,9 @@ getAllAnimeData = async () => {
         titleAnime.setAttribute('data-bs-toggle', "tooltip")
         titleAnime.setAttribute('data-bs-placement', "top")
         titleAnime.setAttribute('data-bs-title', `${animeData.title}`)
-
-
         titleAnime.textContent = animeData.title;
         typeAnime.textContent = animeData.type;
         watchAnime.textContent = "Watch Anime"
-
 
         animeRow.appendChild(colDiv);
         colDiv.appendChild(columnDiv);
@@ -52,10 +58,22 @@ getAllAnimeData = async () => {
 
       })
     }
+    const buildParams = () =>{
+        const params = {
+            q: inputSearch.value
+        }
+        return params;
+    }
 
     window.onload = async () => {
         await getAllAnimeData();
         createElements();
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+        btnSearch.addEventListener("click", async ()=> {
+        const params = buildParams();
+        await getAllAnimeData(params);
+        createElements();
+    })
     }
